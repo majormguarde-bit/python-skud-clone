89fb0f5397c64d88b3b83b05c2c668fb1fb69fca
guard-e <majorm@yandex.ru>
2026-02-01 00:15:04 +0300

    docs: add deployment instructions for Windows

---
+++ run_waitress.py
@@ -0,0 +1,9 @@
+from app import create_app
+from waitress import serve
+import os
+
+app = create_app()
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    serve(app, host='0.0.0.0', port=port)
