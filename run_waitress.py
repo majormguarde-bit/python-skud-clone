5e86d08690722bc7a41839ac3af72850019a0e4a
guard-e <majorm@yandex.ru>
2026-02-01 01:01:39 +0300

    Fix run_waitress.py: move create_app inside main block to avoid NoneType error

--- run_waitress.py
+++ run_waitress.py
@@ -2,8 +2,7 @@ from app import create_app
 from waitress import serve
 import os
 
-app = create_app()
-
 if __name__ == '__main__':
+    app = create_app()
     port = int(os.environ.get("PORT", 5000))
     serve(app, host='0.0.0.0', port=port)
